6d4405de9b46345c9880757b702c17af7d937d94
something-natural <tsglow@naver.com>
2024-09-11 16:30:46 +0900

    feat: done convert fuction using array

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ const output = document.getElementById("result");
 when user input value and click "convert" or press enter
     1. print() call convert(inputNum.value) / done
     2. convet(input) try to parseInt() :
-        - a. check if input is null, or including "e", or negative number
-        - b. if a, return alert("you should enter positive decimal number only")
+        - a. check if input is null, or including "e", or negative number /done
+        - b. if a, return alert("you should enter positive decimal number only") /done
         - c. if not, convert and return result
 */
 
+// return false if value is null or NaN or Negative or including e
 function check(value){    
     if ( !value || isNaN(value) || value < 0 || value.toString() !== inputNum.value){        
         return false;
@@ -20,16 +21,28 @@ function check(value){
 }
 
 
+// 
 function convert(){
-    const val = parseInt(inputNum.value);
+    let val = parseInt(inputNum.value);
+    let result = ""
     if (!check(val)){
         inputNum.value = "";
         output.innerText = "";        
         alert("you should enter positive decimal number only");
         return ;
     } else {
-        output.innerText = val;
-        inputNum.value=""
+        // covert to binary using array        
+        if ( val === 0 || val === 1){
+            result = val.toString();
+        } else {
+            let resultArr = [];
+            while ( val > 0) {
+                resultArr.unshift( val % 2);
+                val = Math.floor(val / 2)
+            }            
+            result = resultArr.join("");
+        }
+        output.innerText = result;        
     }    
 }
 
