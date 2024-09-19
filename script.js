c8196f27f5829b57ce79c1a534e7025a0e64561e
something-natural <tsglow@naver.com>
2024-09-19 11:44:59 +0900

    feat: done animation

--- script.js
+++ script.js
@@ -2,7 +2,34 @@
 const inputNum = document.getElementById("number-input");
 const covBtn = document.getElementById("convert-btn");
 const output = document.getElementById("result");
-
+const aniCon = document.getElementById("animation-container");
+const animationData = [
+    {
+      inputVal: 5,
+      marginTop: 300,
+      addElDelay: 1000,
+      msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+      showMsgDelay: 30000,
+      removeElDelay: 40000,
+    },
+    {
+      inputVal: 2,
+      marginTop: -200,
+      addElDelay: 1500,
+      msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+      showMsgDelay: 20000,
+      removeElDelay: 25000,
+    },
+    {
+      inputVal: 1,
+      marginTop: -200,
+      addElDelay: 2000,
+      msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+      showMsgDelay: 10000,
+      removeElDelay: 15000,
+    }
+  ];
+  
 /*
 when user input value and click "convert" or press enter
     1. print() call convert(inputNum.value) / done
@@ -33,6 +60,39 @@ const convRecurse = (val) => {
     }
 }
 
+const runAnimation = () => {
+    console.log("wow")
+    output.innerText = "Call Stack Animation"
+    animationData.forEach((obj) => {
+        setTimeout(() => {
+            aniCon.innerHTML += `
+            <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px;" class="animation-frame">
+                decimalToBinary(${obj.inputVal})
+            </p>
+            `
+        }, obj.addElDelay
+        )
+
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).textContent = obj.msg;
+        }, obj.showMsgDelay
+        )
+
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).remove();
+        }, obj.removeElDelay
+        )
+
+        setTimeout(() => {
+            output.innerText = convRecurse(5);
+        }, 20000
+        )
+    }
+
+    )
+    
+}
+
 
 // convert using array or recursive function
 function convert(){
@@ -44,8 +104,10 @@ function convert(){
         inputNum.value = "";
         output.innerText = "";        
         alert("you should enter positive decimal number only");
-        return ;
-    } else {
+        return ;        
+    } else if ( val === 5){
+        runAnimation();
+    }else {
         // covert to binary using array        
         /*
         if ( val === 0 || val === 1){
